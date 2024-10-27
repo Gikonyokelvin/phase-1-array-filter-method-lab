eefce471648bc88e730a3ab8d8a22386f4e3b3df
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-27 15:48:17 +0300

    complete filter lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+// function to find drivers with matching names
+const findMatching = function (drivers, name) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase() === name.toLowerCase());  
+};
+
+// function to find drivers with fuzzy matching
+const fuzzyMatch = function (drivers, letters) {
+    return drivers.filter(
+        (driver) => driver.toLowerCase().startsWith(letters.toLowerCase()));
+};
+
+// function to match names
+const matchName = function (drivers, name) {
+    return drivers.filter((driver) => driver.name === name);
+};
